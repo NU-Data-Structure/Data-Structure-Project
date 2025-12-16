7ba83ed3ab962306b2f081fd8be8082d30c266c1
koka-gamal <kokagamal92006@gmail.com>
2025-12-16 13:39:08 +0200

    Added buttons with functionallities in the js and product folder, also stored temporary cart in cpp vector inside main

--- src/server.cpp
+++ src/server.cpp
@@ -28,25 +28,7 @@ customer Server::getProfile(int id) {
     return accounts.getCustomer(id);
 }
 
-// void Server::loadFile(){
-//     rapidcsv::Document doc("../data/Customer_List.csv");
-    
-//     vector<int> id_list= doc.GetColumn<int>("userID");
-//     vector<string> name_list= doc.GetColumn<string>("userName");
-//     vector<string> phone_list= doc.GetColumn<string>("userPhone");
-//     vector<string> address_list= doc.GetColumn<string>("userAddress");
-    
-//     int number_users = doc.GetRowCount();
-    
-//     for (int i = 0; i < number_users; i++)
-//     {
-//         customer c(id_list[i],name_list[i],phone_list[i],address_list[i]);
-//         accounts.addCustomer(c);
 
-//     }
-    
-    
-// }
 void Server::loadFile(){
     rapidcsv::Document doc("data/Customer_List.csv");
     
